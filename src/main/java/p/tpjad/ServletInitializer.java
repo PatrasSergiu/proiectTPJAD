9f994aff859f5ba11bc4df3f771035c2a8528edb
David Pitutiu <56295878+davidpitutiu@users.noreply.github.com>
2023-01-27 16:28:27 +0200

    Added spring boot configs

---
+++ src/main/java/p/tpjad/ServletInitializer.java
@@ -0,0 +1,13 @@
+package p.tpjad;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TpjadApplication.class);
+	}
+
+}
---
+++ src/main/java/p/tpjad/TpjadApplication.java
@@ -0,0 +1,13 @@
+package p.tpjad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpjadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpjadApplication.class, args);
+	}
+
+}
